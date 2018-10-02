75378e778188a614ce406585fcde9cb8dd62f60d
Dave Hewy <dave@bytewire.co.uk>
2018-10-02 13:58:15 +0100

    add tf.sh additions

--- bin/tf.sh
+++ bin/tf.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-echo "wrap terraform"
+action=$1
+workspace=$2
+region=$3
+s3_bucket='example-terraform-state-files'
+
+echo "Action: ${1}"
+echo "Workspace: ${2}"
+echo "Region: ${3}"
+
+cd ../
+
+# symlink in the component backend.
+ln -sf ../../components/backend.tf ./terraform/environments/$workspace/
+
+# shift into workspace
+cd terraform/environments/$workspace
+rm -rf .terraform
+
+terraform init -backend-config "bucket=${s3_bucket}" -backend-config "key=environments/${workspace}.tfstate" -backend-config "region=${region}"
+
+# jump into the correct workspace to be sure
+# terraform workspace select $workspace terraform/environments/$workspace
+
+# Do all manner of other things
+# - Error checking?
+# - tfvar file loading
+
+
+terraform $action
+
+# shift back
+cd -
