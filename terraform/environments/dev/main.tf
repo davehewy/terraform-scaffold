95ef4f2fa1133f17e4c44975e542412bd103361c
Dave Hewy <dave@bytewire.co.uk>
2018-10-02 13:59:19 +0100

    extend the sample out fully

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "web_instance" {
   instance_type = "${var.instance_type}"
   ami = "ami-0bdb1d6c15a40392c"
+  subnet_id = "subnet-620d8d15"
+  tags = {
+    Name = "test.terraform.instance"
+  }
 }
